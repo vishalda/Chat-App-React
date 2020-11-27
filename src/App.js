84ea9dbdd58ae2d36f716d455839427c73f5c956
VDA-001 <vdavishu@gmail.com>
2020-11-27 16:33:31 +0530

    UI part updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+// import { Picker } from 'emoji-mart'
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -7,17 +8,8 @@ import 'firebase/auth';
 
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
+import './ApiKey'
 
-firebase.initializeApp({
-  apiKey: "AIzaSyB3mTGN8n54C7L282b1MBN8UKsKB8vifV8",
-    authDomain: "chat-app-react-bc939.firebaseapp.com",
-    databaseURL: "https://chat-app-react-bc939.firebaseio.com",
-    projectId: "chat-app-react-bc939",
-    storageBucket: "chat-app-react-bc939.appspot.com",
-    messagingSenderId: "604614219137",
-    appId: "1:604614219137:web:7575c0f58ff7be60cd0480",
-    measurementId: "G-1FLS09Q6ZW"
-})
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -29,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>⚛️🔥💬</h1>
+        <h1>⚛️</h1>
         <SignOut />
       </header>
       <section>
@@ -62,16 +54,23 @@ function ChatRoom(){
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    const {uid} = auth.currentUser;
+    const {uid, photoURL} = auth.currentUser;
 
     await messageRef.add({
       text:formValue,
       createdAt:firebase.firestore.FieldValue.serverTimestamp(),
       uid,
+      photoURL
     })
     setFormValue('');
   }
 
+  // const addEmoji = e =>{
+  //   let emoji = e.native;
+  //   setFormValue({
+  //     text: formValue+emoji
+  //   });
+  // };
   const dummy = useRef();
   useEffect(()=>{
     dummy.current.scrollIntoView({behaviour:'smooth'});
@@ -87,16 +86,20 @@ function ChatRoom(){
         <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder = "say something nice" />
         <button type="submit" disabled={!formValue}>Send</button>
       </form>
+      {/* <span>
+        <Picker onSelect={addEmoji} />
+      </span> */}
     </>
   )
 }
 function ChatMessage(props){
-  const {text, uid} = props.message;
+  const {text, uid, photoURL} = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recieve';
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
     <div className={`message ${messageClass}`} > 
+      <img src={photoURL} />
       <p>{text}</p>
     </div>
   )
